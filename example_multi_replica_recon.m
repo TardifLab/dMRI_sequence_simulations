c5cd90ab605257bf6410e80394dbe8e59c841cf9
sajjad-feizollah <76600877+sajjad-feizollah@users.noreply.github.com>
2022-07-12 13:28:28 -0400

    Update example_multi_replica_recon.m

--- example_multi_replica_recon.m
+++ example_multi_replica_recon.m
@@ -1,2 +1,29 @@
+% -_-_-_-_-_-_-_-_-_-_-_-_-_-_-example_multi_replica_recon-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+%
+% Description: 
+% -----------
+% 
+% Example code for multi replica method to calculate SNR of a scan.
+% Sample data is available at:
+% 
+% Requirements:
+% ------------
+% 
+% For image reconstruction, expanded signal model implementation is needed
+% that can be found at: github.com/TardifLab/ESM_image_reconstruction
+% 
+% **Requires a GPU to simulate image acquisition**
+%
+% Article: Feizollah and Tardif (2022)
+% -------
+%
+% Sajjad Feizollah, July 2022
+% -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
 clear
 clc
+
+data_adrs='';
+map_adrs='';
+
+im_replicas=snr_multi_replica_pipe(data_adrs,map_adrs,100,8)
